7c83a9df334dc320b6579472ffb17c0b4db2d4d9
noremacsim <mrcameronsim@gmail.com>
2023-07-01 09:38:52 +0100

    Add Support for the chrome extension

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,31 @@
 
   <main>
 
-    <UserHeader v-if="!userLoading" />
-    <UserHeaderSkeleton v-if="userLoading" />
+    <UserHeader v-if="!userLoading && showMain" />
+    <UserHeaderSkeleton v-if="userLoading && showMain" />
 
     <div class="appContainer" role="main">
-      <UserAppList v-if="!userLoading"/>
-      <UserAppListSkeleton v-if="userLoading" />
+      <UserAppList v-if="!userLoading && showMain"/>
+      <UserAppListSkeleton v-if="userLoading && showMain" />
+    </div>
+
+    <div class="card" v-show="!showMain">
+      <div class="card-body" style="background: #f2f2f2">
+        <h5 style="color: black;font-weight: bold;text-align: center;">Your Fossa ID</h5>
+
+        <div class="form-group mb-2">
+
+          <div class="ui-textinput ui-corner-all ui-shadow-inset ui-textinput-text ui-body-inherit">
+            <div class="ui-textinput ui-corner-all ui-shadow-inset ui-textinput-text ui-body-inherit currentAppIdCode">
+              <div class="ui-textinput ui-corner-all ui-shadow-inset ui-textinput-text ui-body-inherit">
+                <input :value="user.uniqueID" type="text" class="form-control rounded appId" placeholder="Code" aria-label="Code" id="appid" name="appId" style="width: 210px;margin: auto;text-align: center;background: transparent;background: #54b4d3;border: none;font-size: 22px;font-weight: bold;border-radius: 25px !important;letter-spacing: 3px;color: white;box-shadow: 0 4px 9px -4px #54b4d3;margin-top: 16px;" readonly=""></div>
+            </div>
+          </div>
+          <div style="margin-top:10px;text-align: center;width: 100%;">
+            <span>Copy and paste this into your chrome extension to load your profile</span>
+          </div>
+        </div>
+      </div>
     </div>
 
   </main>
@@ -23,15 +42,20 @@ import UserHeader from "@/components/UserHeader.vue";
 import UserHeaderSkeleton from "@/components/skeleton/UserHeaderSkeleton.vue";
 import UserAppListSkeleton from "@/components/skeleton/UserAppListSkeleton.vue";
 
-import {defineEmits} from 'vue'
+import {defineEmits, ref} from 'vue'
 const emit = defineEmits(['showSettings'])
 
 const { userLoading } = storeToRefs(useUserStore());
+const { user } = storeToRefs(useUserStore());
 const { initUser, importUserFromAppID } = useUserStore();
 
 const importedCode = location?.search?.split('code=')[1] ?? '';
+const chromeExtension = location?.search?.split('chrome=')[1] ?? '';
+const showMain = ref(true);
 
-if (importedCode && importedCode !== '') {
+if (chromeExtension && chromeExtension != '') {
+  showMain.value = false;
+} else if (importedCode && importedCode !== '') {
   importUserFromAppID(importedCode).then(() => {
     initUser().then(success => {
       if (success === false) {
