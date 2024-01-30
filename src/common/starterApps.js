55225e711a79ba6aa920e63fffb7ea672612e575
noremacsim <mrcameronsim@gmail.com>
2024-01-30 19:17:17 +0000

    Update Logo

--- src/common/starterApps.js
+++ src/common/starterApps.js
@@ -1,11 +1,10 @@
 export const starterApps = [
     { name: 'Plex', url: 'https://app.plex.tv/desktop/#!/', image: 'https://images-eds-ssl.xboxlive.com/image?url=Q_rwcVSTCIytJ0KOzcjWTYl.n38D8jlKWXJx7NRJmQKBAEDCgtTAQ0JS02UoaiwRsDCcLS6OFHFfrsNxLIIAjQwPuLt5MeFHqrz4XXCpqzcCRRBS2QYjQOSI9idbPia8lKLwBy7waJxbm1H.ZuG8PQ--&format=source&w=120' },
     { name: 'Paramount+', url: 'https://www.paramountplus.com/gb/', image: 'https://store-images.s-microsoft.com/image/apps.14266.9007199266243596.1230f3f2-f93f-4f0e-8de0-9e19b3939e30.3f46be48-a90d-44e5-a76c-9969b7d89625?w=120' },
-    { name: 'Prime Video', url: 'https://primevideo.com', image: 'https://www.fullscreentesla.com/static/media/prime.0563c23e38eaa072810d.png' },
+    { name: 'Prime Video', url: 'https://primevideo.com', image: '/img/logos/image_1863.png' },
     { name: 'Disney Plus', url: 'https://www.disneyplus.com/en-gb', image: 'https://store-images.s-microsoft.com/image/apps.6899.14495311847124170.e89a4dce-fd9a-4a10-b8e4-a6c3aa1c055e.bcea8b9e-9a72-45eb-a2fc-e265b7dc1915' },
     { name: 'Audible', url: 'https://www.audible.co.uk/', image: 'https://images-eds-ssl.xboxlive.com/image?url=rOOPPWX9ohGfLksSzq1Wig8iTZ05uPbeb.DpV4tdbbbWNm1XXu8uEEIOyHxIQzbkg6QV19S8ccQ3rJJvQNmSUn_TC6y.r4fl_f9KOa1NN_xM_dIQYqurJp4Hx5.6pahDol7yzO6kUj5dDpUHeMO8DeUy4j1j3nWAb3FpbQDy62FObum0F9nvjLKgT_Xovk6lEkvYVCwGudSAr0ZaQiL.BoKzHUwhhxtyLtaqdFaD8S65fm.zpj1PrB23CYmbZAyQYGQY6Ipf.W49JAVUYSvJZGIXe6vyxKR_vL_l.TxWmg9U2Q044vQJft1fT6L4.VqT&format=source&w=120' },
     { name: 'Amazon Music', url: 'https://music.amazon.co.uk/', image: 'https://store-images.s-microsoft.com/image/apps.20311.13756631990520576.c7bf8936-a11d-4c89-b498-2442c2977baf.d9909661-86c4-4ca8-b820-20101dd12a50?w=120' },
-    { name: 'Prime Video', url: 'https://primevideo.com', image: 'https://www.fullscreentesla.com/static/media/prime.0563c23e38eaa072810d.png' },
     { name: 'HBO Max', url: 'https://www.hbomax.com/', image: 'https://hbomax-images.warnermediacdn.com/2020-05/square%20social%20logo%20400%20x%20400_0.png' },
     { name: 'Xbox Cloud Gaming', url: 'https://www.xbox.com/en-us/play', image: 'https://images.macrumors.com/t/pT92j2c3I_lhW8OzErMWxS6FA1w=/1200x1200/smart/article-new/2020/12/xbox-logo.jpeg' },
     { name: 'Jetpack JoyRide', url: 'https://play.famobi.com/jetpack-joyride/A1000-10', image: 'https://img.cdn.famobi.com/portal/html5games/images/tmp/JetpackJoyrideTeaser.jpg?v=0.2-093a2fd2' },
