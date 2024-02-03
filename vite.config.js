c031f7ab4f5f02d68b25a098772a7f805ce9b115
noremacsim <mrcameronsim@gmail.com>
2024-02-03 13:53:27 +0000

    remove hash from files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
-const hash = Math.floor(Math.random() * 90000) + 10000;
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -76,9 +75,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `[name]${hash}.js`,
-        chunkFileNames: `[name]${hash}.js`,
-        assetFileNames: `[name]${hash}.[ext]`
+        entryFileNames: `[name].js`,
+        chunkFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`
       }
     }
   },
